219f38bba3f829d74234cfa9ce1561ed8f2ff76c
Hammad-Hasnain <hammadhasnain81@gmail.com>
2026-08-26 00:42:56 +0500

    feat: integrate @nestjs/config and @nestjs/mongoose for environment configuration and MongoDB connection; update package dependencies

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { DriversModule } from './domains/drivers/drivers.module';
 import { OrdersModule } from './domains/orders/orders.module';
 import { TrackingModule } from './domains/tracking/tracking.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DriversModule, OrdersModule, TrackingModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    DriversModule,
+    OrdersModule,
+    TrackingModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
